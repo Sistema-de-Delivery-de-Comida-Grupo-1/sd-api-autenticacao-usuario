9ea66396c807cd0df64038bf131624f22529d7ec
Cristian <cristianmarfer14@gmail.com>
2026-05-27 09:16:36 -0300

    Alterações finais

--- microsservico-autenticacao/src/main/java/br/ifg/urutai/microsservicoautenticacao/service/AuthServiceImpl.java
+++ microsservico-autenticacao/src/main/java/br/ifg/urutai/microsservicoautenticacao/service/AuthServiceImpl.java
@@ -1,35 +1,39 @@
-//package br.ifg.urutai.microsservicoautenticacao.service;
-//
-//import net.devh.boot.grpc.server.service.GrpcService;
-//
-//@GrpcService
-//public class AuthServiceImpl extends AuthServiceGrpc.AuthServiceImplBase {
-//
-//    @Override
-//    public void validarLogin(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
-//        String email = request.getEmail();
-//        String senha = request.getSenha();
-//
-//        System.out.println(">>> Requisição gRPC recebida para o e-mail: " + email);
-//
-//        LoginResponse response;
-//
-//        // Validação acadêmica simples em texto plano
-//        if (senha.equals("senhaSegura123")) {
-//            response = LoginResponse.newBuilder()
-//                    .setSucesso(true)
-//                    .setToken("TOKEN_JWT_SIMULADO_VALIDEZ_OK_123")
-//                    .setMensagem("Autenticado com sucesso")
-//                    .build();
-//        } else {
-//            response = LoginResponse.newBuilder()
-//                    .setSucesso(false)
-//                    .setToken("")
-//                    .setMensagem("Credenciais inválidas")
-//                    .build();
-//        }
-//
-//        responseObserver.onNext(response);
-//        responseObserver.onCompleted();
-//    }
-//}
+package br.ifg.urutai.microsservicoautenticacao.service;
+
+import br.ifg.urutai.microsservicoautenticacao.grpc.LoginRequest;
+import br.ifg.urutai.microsservicoautenticacao.grpc.LoginResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class AuthServiceImpl extends br.ifg.urutai.microsservicoautenticacao.grpc.AuthServiceGrpc.AuthServiceImplBase {
+
+    @Override
+    public void validarLogin(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
+        String email = request.getEmail();
+        String senhaDigitada = request.getSenhaDigitada();
+        String senhaDoBanco = request.getSenhaDoBanco();
+
+        System.out.println(">>> Validando credenciais gRPC para: " + email);
+
+        LoginResponse response;
+
+        // Comparação dinâmica baseada no banco de dados H2
+        if (senhaDigitada.equals(senhaDoBanco)) {
+            response = LoginResponse.newBuilder()
+                    .setSucesso(true)
+                    .setToken("TOKEN_JWT_DINAMICO_OK_" + email.hashCode())
+                    .setMensagem("Autenticado com sucesso!")
+                    .build();
+        } else {
+            response = LoginResponse.newBuilder()
+                    .setSucesso(false)
+                    .setToken("")
+                    .setMensagem("Senha incorreta.")
+                    .build();
+        }
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
